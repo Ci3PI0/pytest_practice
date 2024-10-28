78195fad1807d1e79a95203801ab6ffadd2f8fba
Meirman <Meyirmant@gmail.com>
2024-10-28 19:27:35 +0500

    add order

--- Pytest_topics/py_fixtures/test_fixtures05.py
+++ Pytest_topics/py_fixtures/test_fixtures05.py
@@ -16,12 +16,18 @@ def test_fixture_params01(fixture01):
     # assert fixture01 == [3,5]
     assert fixture01 == (3,4)
 
+
+@pytest.mark.order(2)
 @pytest.mark.parametrize('_str',[1,2,3,4,5,6,True,'false'])
 def test_num_data_type(_str):
+    print('Order 2')
+    print(_str)
     assert type(_str) == int
 
 # @pytest.mark.xfail()
+@pytest.mark.order(1)
 def test_fix_param02(fixture01,fixture02):
+    print('Order 1')
     if fixture02 == 'access':
         # print(fixture01[2])
         with pytest.raises(IndexError):
